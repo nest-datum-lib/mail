d55f7a1cc59bea471a74e70903e5b642390d1914
ihor <ihor.bielchenko@gmail.com>
2022-12-23 00:53:42 +0100

    test

--- src/api/letter/letter.controller.ts
+++ src/api/letter/letter.controller.ts
@@ -136,6 +136,8 @@ export class LetterController {
 	@EventPattern('letter.send')
 	async send(payload) {
 		try {
+			console.log('payload', payload);
+			
 			const output = await this.letterService.send({
 				user: Validators.token('accessToken', payload['accessToken'], {
 					accesses: [ process['ACCESS_MAIL_LETTER_CREATE'] ],
